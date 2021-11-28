fddea53dfaf394919eeb658c1fe01319d24673b4
Martin Panzer <postremus1996@googlemail.com>
2021-11-28 20:20:58 +0100

    Implement simple FileSystem compatibility layer between jdk 14 and jdk 11 In jdk 13 and 14, a lot of new helper methods emerged for the filesystems class. These made possible to construct fs with path and env. This MR Jar makes it possible to always specifiy the env, but ignores them on jdk < 14

---
+++ src/main/java/com/pro_crafting/tools/FileSystemHelper.java
@@ -0,0 +1,18 @@
+package com.pro_crafting.tools;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.Map;
+
+public class FileSystemHelper {
+    public static FileSystem openFS(Path path, Map<String, ?> env, ClassLoader classLoader) throws IOException {
+        return FileSystems.newFileSystem(path, classLoader);
+    }
+
+    public static FileSystem openFS(URI uri, Map<String, ?> env, ClassLoader classLoader) throws IOException {
+        return FileSystems.newFileSystem(uri, env, classLoader);
+    }
+}
